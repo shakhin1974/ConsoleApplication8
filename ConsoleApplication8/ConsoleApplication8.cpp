e43769f86445ce9751769a785d93ad4b51a8c2c7
Shakhin Nabiev <shakhin@mail.ru>
2024-03-07 09:53:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+struct Student
+{
+ 
+	int id{};
+	string fio{};
+	string gruppa{};
+private:
+	char pol{};
+	int year{};
+	int stependia{};
+public:
+	void Gender(char gen)
+	{
+		gen = pol;
+	}
+	char Gen(char gen)
+	{
+		return gen;
+	}
+};
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Student Ivan;
+	Ivan.id =  1 ;
+	Ivan.fio =  "Иванов Иван" ;
+	Ivan.gruppa = { "2ИСИП -222" };
+	Ivan.Gender('М');
+	 
+}
